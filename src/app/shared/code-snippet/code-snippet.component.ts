8d5191d785104fd9cb5fd4a4eb57c5d0fe7dcd35
NailethR <nait.0005@gmail.com>
2025-04-03 16:50:07 -0300

    Generar componentes principales

---
+++ src/app/shared/code-snippet/code-snippet.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-code-snippet',
+  imports: [],
+  templateUrl: './code-snippet.component.html',
+  styleUrl: './code-snippet.component.scss'
+})
+export class CodeSnippetComponent {
+
+}
